1ec4511abaced515be0d5b1776cdf2afc4a3b806
Guillaume Cadet <guiguilebandit@live.fr>
2023-09-27 23:01:16 +0200

    Repair merge regression

--- streamlit_app/config.py
+++ streamlit_app/config.py
@@ -2,20 +2,34 @@
 
 Config file for Streamlit App
 
+Note: typo convension: global variables are uppercase
+
 """
 
 from member import Member
 
 
-TITLE = "My Awesome App"
+TITLE = "Mushroom Classification"
 
 TEAM_MEMBERS = [
     Member(
-        name="John Doe",
-        linkedin_url="https://www.linkedin.com/in/charlessuttonprofile/",
-        github_url="https://github.com/charlessutton",
+        name = "Justine Mialhe",
+        linkedin_url = "https://www.linkedin.com/in/charlessuttonprofile/",
+        github_url = "https://github.com/charlessutton"
     ),
-    Member("Jane Doe"),
+    Member(
+        name = "Guillaume Pot",
+        linkedin_url = "https://www.linkedin.com/in/charlessuttonprofile/",
+        github_url = "https://github.com/charlessutton"
+    ),
+    Member(
+        name = "Guillaume Cadet",
+        linkedin_url = "https://www.linkedin.com/in/guillaume-cadet-387b61100/",
+        github_url = "https://github.com/gcadet2016"
+    )        
 ]
 
-PROMOTION = "Promotion Bootcamp Data Scientist - April 2021"
+PROMOTION = "Promotion Bootcamp Data Scientist - July 2023"
+
+INFOS_IMAGES_PATH = '../data/infos_images.csv'
+TOP10_PATH = '../data/top10.csv'
